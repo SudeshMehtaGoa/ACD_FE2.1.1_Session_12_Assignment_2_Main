bf6c8c4f58b5ea00dc70b0414da76dbc3918abac
Mehta Sudesh <Mehta@iazi.ch>
2017-08-01 06:58:23 +0200

    session 12 assignment 2

---
+++ src/app/routes/myapp.routes.ts
@@ -0,0 +1,15 @@
+/* Router file */
+import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from 'app/component/student-component/student.component';
+import { StudentListComponent } from "app/component/student-list/student-list.component";
+import { PageNotFoundComponent } from 'app/component/page-not-found/page-not-found.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/student', pathMatch: 'full' },
+  { path: 'student', component: StudentComponent },
+  { path: 'studentList', component: StudentListComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+export const appRouting = RouterModule.forRoot(routes);
